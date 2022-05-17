0c74a9379c320653eb9b50d92332675d7d3223aa
prashant parajuli <prashanta.parajuli@macpokhara.edu.np>
2022-05-17 20:16:59 +0545

    models updated

--- models/todos.js
+++ models/todos.js
@@ -9,12 +9,9 @@ const todosSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    status: {
-        type: [{
-            type: String,
-            enum: ['pending', 'ongoing', 'completed']
-        }],
-        default: ['pending'],
+    completed: {
+        type: Boolean,
+        default: false,
     },
 })
 module.exports = mongoose.model('Todos', todosSchema);
