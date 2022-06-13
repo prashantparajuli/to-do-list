1a9116bf5bedc88c4002cc61022033243bdeadc8
prashant parajuli <prashanta.parajuli@macpokhara.edu.np>
2022-06-13 13:53:16 +0545

    pagination updated

--- middlewares/paginatedResult.js
+++ middlewares/paginatedResult.js
@@ -1,7 +1,7 @@
 module.exports = function paginatedResult(model) {
     return async(req, res, next) => {
-        const page = parseInt(req.query.page)
-        const limit = parseInt(req.query.limit)
+        const page = (req.query.page) ? parseInt(req.query.page) : 1;
+        const limit = (req.query.limit) ? parseInt(req.query.limit) : 3;
 
         const startIndex = (page - 1) * limit
         const endIndex = page * limit
